bdead82b4c5f2ae9541061957352bc37dc4a254c
thuds <hudson_thom>
2020-02-06 23:02:06 -0500

    cs280 hw2

---
+++ cs280_hw2/src/TestMultiItemSale.java
@@ -0,0 +1,7 @@
+public class TestMultiItemSale {
+    public static void main(String[] args){
+        MulitItemSale newMultiSale = new MulitItemSale(10);
+        newMultiSale.addItems();
+        newMultiSale.checkOut();
+    }
+}
