65be6f07d428da8d9c515fadbfac20a37382c544
thuds <hudson_thom>
2020-02-23 13:10:52 -0500

    Removed testing files

--- cs280_hw2/src/TestMultiItemSale.java
+++
@@ -1,7 +0,0 @@
-public class TestMultiItemSale {
-    public static void main(String[] args){
-        MulitItemSale newMultiSale = new MulitItemSale(10);
-        newMultiSale.addItems();
-        newMultiSale.checkOut();
-    }
-}
