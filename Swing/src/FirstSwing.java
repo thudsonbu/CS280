811c2c298cdf57051db72012cd02006f8c8823bd
thudsonbu <hudson_thom@bentley.edu>
2020-04-01 13:43:01 -0600

    Moved window to seperate class

--- Swing/src/FirstSwing.java
+++ Swing/src/FirstSwing.java
@@ -1,11 +1,7 @@
-import javax.swing.*;
-import javax.swing.JButton;
 
 public class FirstSwing {
     public static void main(String[] args){
-        JFrame firstWindow = new JFrame();
-        firstWindow.setSize(200,200);
-        firstWindow.setVisible(true);
-
+        SecondSwing w = new SecondSwing();
+        w.setVisible(true);
     }
 }
---
+++ Swing/src/SecondSwing.java
@@ -0,0 +1,25 @@
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SecondSwing  extends JFrame {
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 200;
+
+    public SecondSwing() {
+        super();
+        setSize(WIDTH, HEIGHT);
+        setTitle("First Window Class");
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        JButton endButton = new JButton("Click to end program.");
+        endButton.addActionListener(new EndingListener());
+        add(endButton);
+    }
+
+    public class EndingListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+}
