12d26db445d7bbc877bd478832018d382d0008e7
thudsonbu <hudson_thom@bentley.edu>
2020-04-01 13:19:10 -0600

    Taking a swing at Swing

---
+++ Swing/src/FirstSwing.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import javax.swing.JButton;
+
+public class FirstSwing {
+    public static void main(String[] args){
+        JFrame firstWindow = new JFrame();
+        firstWindow.setSize(200,200);
+        firstWindow.setVisible(true);
+
+    }
+}
