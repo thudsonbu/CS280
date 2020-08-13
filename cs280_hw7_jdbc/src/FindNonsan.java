16af1662b0f9d1cf1ef77edd6f9471831875cd1e
thudsonbu <hudson_thom@bentley.edu>
2020-08-13 16:28:54 -0600

    Updated to use current JDBC driver

--- cs280_hw7_jdbc/src/FindNonsan.java
+++ cs280_hw7_jdbc/src/FindNonsan.java
@@ -14,7 +14,7 @@ public class FindNonsan {
 
         // Load MySQL driver into the JVM memory
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         }
         catch (ClassNotFoundException e) {
             System.out.println("Failed to lad MySQL drive.");
